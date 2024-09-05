ce3135375377b0fedaa151131b8dba9cc0e72e1d
Alexander <sanya.oleinickov22@yandex.ru>
2024-09-05 10:44:07 +0300

    Changed for the depth camera

--- camera.py
+++ camera.py
@@ -65,8 +65,8 @@ class Camera:
         self.pipeline = rs.pipeline()
 
         config = rs.config()
-        config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 15)
 
         self.color_frame = None
         self.depth_frame = None
